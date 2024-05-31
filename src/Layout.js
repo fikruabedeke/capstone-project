b65d05ea29cc6d055a1583666c2ec32eed2f2b09
Bedeke <45323366+fikruabedeke@users.noreply.github.com>
2024-05-31 19:02:59 -0400

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -12,7 +12,7 @@ const Layout = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-            <Link to="/create" className="nav-link active" aria-current="page" href="#createAccount" id="createAccount">CreateAccount</Link>
+            <Link to="/create" className="nav-link active" aria-current="page" id="createAccount">CreateAccount</Link>
             </li>
             <li className="nav-item">
             <Link to="/login" className="nav-link" id="login">Login</Link>
