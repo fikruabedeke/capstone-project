dd91ce8179066ef02f5bb12b1e4163126b94b53e
Bedeke <45323366+fikruabedeke@users.noreply.github.com>
2024-05-31 11:38:23 -0400

    updated dbsample don js
    c

--- dbsample.js
+++ dbsample.js
@@ -1,4 +1,4 @@
-    const { MongoClient } = require('mongodb');
+    /*const { MongoClient } = require('mongodb');
 
     const url = 'mongodb://localhost:27017';
     const client = new MongoClient(url);
@@ -30,4 +30,4 @@
     main()
       .then(console.log)
       .catch(console.error)
-      .finally(() => client.close());
+      .finally(() => client.close());*/
