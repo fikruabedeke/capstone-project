e5a40f2b7c91a10184fc5db7d84dcc50cc4ccf00
Bedeke <45323366+fikruabedeke@users.noreply.github.com>
2024-05-31 11:48:31 -0400

    removed onclick from layout file

--- src/Layout.js
+++ src/Layout.js
@@ -5,29 +5,29 @@ const Layout = () => {
     <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link to="/" className="navbar-brand" onClick="defaultModule()">Home</Link>
+          <Link to="/" className="navbar-brand">Home</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-            <Link to="/create" className="nav-link active" aria-current="page" href="#createAccount" onClick="loadCreateAccount()" id="createAccount">CreateAccount</Link>
+            <Link to="/create" className="nav-link active" aria-current="page" href="#createAccount" id="createAccount">CreateAccount</Link>
             </li>
             <li className="nav-item">
-            <Link to="/login" className="nav-link" onclick="loadLogin()" id="login">Login</Link>
+            <Link to="/login" className="nav-link" id="login">Login</Link>
             </li>
             <li className="nav-item">
-            <Link to="/deposit" className="nav-link" onclick="loadDeposit()" id="deposit">Deposit</Link>
+            <Link to="/deposit" className="nav-link" id="deposit">Deposit</Link>
             </li>
             <li className="nav-item">
-              <Link to="/balance" className="nav-link" onclick="loadBalance()" id="balance">Balance</Link>
+              <Link to="/balance" className="nav-link" id="balance">Balance</Link>
             </li>
             <li className="nav-item">
-              <Link to="/withdraw" className="nav-link" onclick="loadWithdrawal()" id="withdraw">Withdraw</Link>
+              <Link to="/withdraw" className="nav-link" id="withdraw">Withdraw</Link>
             </li>
             <li className="nav-item">
-              <Link to="/alldata" className="nav-link" onclick="loadAllData()" id="alldata">AllData</Link>
+              <Link to="/alldata" className="nav-link" id="alldata">AllData</Link>
             </li> 
             <li className="nav-item dropdown">
                 <Link to="/select" className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Select</Link>
